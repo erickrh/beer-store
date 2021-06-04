781ff3a2316f62d1abf51ea1def365a8618cd78a
Erick Riaño <123erickrh@gmail.com>
2021-06-04 18:01:37 -0500

    Change in a webpack comment

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "js/index.[contenthash].js",
-    assetModuleFilename: "img/[name][ext][query]", // Imagenes en folder.
+    assetModuleFilename: "img/[name][ext][query]", // Images in folder.
   },
   plugins: [
     new HtmlWebpackPlugin({
