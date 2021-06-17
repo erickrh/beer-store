f66eab8778bbc0074924866a5387cf2502b85f1d
Erick Riaño <123erickrh@gmail.com>
2021-06-17 17:12:09 -0500

    Rename custom Tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,22 +7,24 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                customBlue: '#0000A3',
-                customBlack: '#0F0F0F'
+                primary: {
+                    blue: '#0000A3',
+                    black: '#0F0F0F'
+                }
             },
             height: {
-                headerHeight: '70px',
-                axiaHeight: '30px',
-                cardHeight: '72px',
-                arrowHeight: '38px',
-                igHeight: '34px',
-                beerCardHeight: '297px'
+                header: '70px',
+                brand: '30px',
+                card: '72px',
+                arrow: '38px',
+                instagram: '34px',
+                beerCard: '297px'
             },
             width: {
-                axiaWidth: '43px',
-                arrowWidth: '38px',
-                igWidth: '34px',
-                beerCardHeightWidth: '165px'
+                brand: '43px',
+                arrow: '38px',
+                instagram: '34px',
+                beerCard: '165px'
             },
         },
     },
