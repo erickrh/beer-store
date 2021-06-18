0b1471fff950b9f51ccaf642bacb335249a4e078
Erick Riaño <123erickrh@gmail.com>
2021-06-18 17:07:29 -0500

    JSON file added and configured to work with handlebars and Webpack. Beer list added. Some improved contents.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,9 @@ module.exports = {
             colors: {
                 primary: {
                     blue: '#0000A3',
-                    black: '#0F0F0F'
+                    black: '#0F0F0F',
+                    gray: '#E5E5E5',
+                    lightgray: '#F4F4F4'
                 }
             },
             height: {
@@ -18,14 +20,28 @@ module.exports = {
                 card: '72px',
                 arrow: '38px',
                 instagram: '34px',
-                beerCard: '297px'
+                beerCard: '278px',
+                beerImg: '140px',
+                btnAdd: '35px'
             },
             width: {
                 brand: '43px',
                 arrow: '38px',
                 instagram: '34px',
-                beerCard: '165px'
+                beerCard: '165px',
+                beerImg: '137px'
             },
+            padding: {
+                container: '15px'
+            },
+            margin:{
+                container: '15px',
+                distancePrice: '12px'
+            },
+            fontSize: {
+                tiny: "11px",
+                sizeLetter: "14.16px"
+            }
         },
     },
     variants: {
