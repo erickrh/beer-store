bbc7c6066069c81426c4f3a77b1cc6e54c7fd2ed
Erick Riaño <123erickrh@gmail.com>
2021-06-23 15:31:51 -0500

    Final commit

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ function importAll(r) {
 }
 const images = importAll(require.context('./img', false, /\.(png|jpe?g)$/));
 
-
 /*
 const divImg = document.querySelector('.divImg');
 const camba = new Image();
@@ -19,8 +18,8 @@ camba.src = images['camba.png'];
 divImg.appendChild(camba);
 */
 
-const myTemplate = require("./partials/beerCards.handlebars");
-
+// List beers
+const myTemplate = require('./partials/beerCards.handlebars');
 const ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', './beers.json');
 ourRequest.onload = () => {
@@ -29,12 +28,23 @@ ourRequest.onload = () => {
         createHTML(data);
     }
 };
-
 ourRequest.onerror = () => console.log('Connection error');
-
 ourRequest.send();
-
 const createHTML = (beerListData) => {
     const beerContainer = document.getElementById('beerContainer');
     beerContainer.innerHTML = myTemplate(beerListData);
 }
+
+// Modal
+const modalTemplate = require('./partials/modal.handlebars');
+document.addEventListener('DOMContentLoaded', () => {
+    const div = document.createElement('div');
+    div.innerHTML = modalTemplate();
+    document.body.appendChild(div);
+});
+
+// Despedida
+const despedida = () => {
+    console.log(`%cGracias por la oportunidad de haber hecho parte de su equipo. By: Erick.`, 'color: white; background-color: red; padding: 10px;');
+}
+despedida();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     purge: {
-        enabled: false,
+        enabled: true,
         content: ['./src/**/*.html', './src/**/*.handlebars'],
     },
     darkMode: false, // or 'media' or 'class'
@@ -14,7 +14,8 @@ module.exports = {
                     blue: '#0000A3',
                     black: '#0F0F0F',
                     gray: '#E5E5E5',
-                    lightgray: '#F4F4F4'
+                    lightgray: '#F4F4F4',
+                    white: '#FFFFFF'
                 }
             },
             height: {
@@ -23,30 +24,43 @@ module.exports = {
                 card: '72px',
                 arrow: '38px',
                 instagram: '34px',
-                btnAdd: '35px'
+                btnAdd: '35px',
+                filter: '18px',
+                filterCard: '284px'
             },
             width: {
                 brand: '43px',
                 arrow: '38px',
-                instagram: '34px'
+                instagram: '34px',
+                modal: '123px',
+                filter: '18px'
             },
             spacing: {
                 gutter: '15px',
-                brand: '15.5px'
+                brand: '15.5px',
+                modal: '10px',
+                smallSpace: '7.5px',
+                modalInside: '13.5px',
+                cleanDistance: '9.5px',
+                cleanxDistance: '54px'
             },
             margin: {
-                distance: '18px'
+                distance: '18px',
+                filter: '10.75px'
             },
             fontSize: {
                 tiny: '11px',
                 sizeLetter: '14.16px',
                 smallLetter: '0.738rem',
-                wine: '1.53rem'
+                wine: '1.53rem',
+                filter: '20.4px'
             },
         },
     },
     variants: {
-
+        extend: {
+            backgroundColor: ['checked'],
+        }
     },
     plugins: [
 
